604d21270417445709229ad38d7409eca6a0a137
nero <233401861@qq.com>
2026-09-19 23:50:26 +0800

    docs: add commercial consultation to Java API comments

--- backend/src/main/java/cn/zhuatech/cronops/ZhuaTechCronOpsApplication.java
+++ backend/src/main/java/cn/zhuatech/cronops/ZhuaTechCronOpsApplication.java
@@ -1,2 +1,8 @@
 /* Copyright 2026 上海如静知华信息科技有限公司 · https://www.zhuatech.cn/ */
-package cn.zhuatech.cronops;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplication public class ZhuaTechCronOpsApplication{public static void main(String[]a){SpringApplication.run(ZhuaTechCronOpsApplication.class,a);}}
+package cn.zhuatech.cronops;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;/**
+                                                                                                                                                   * 商业授权或定制开发请微信添加微信号zhuatech或zhuatech2进行咨询。
+                                                                                                                                                   */
+@SpringBootApplication public class ZhuaTechCronOpsApplication{/**
+                                                                                                                                                                                                                  * 商业授权或定制开发请微信添加微信号zhuatech或zhuatech2进行咨询。
+                                                                                                                                                                                                                  */
+public static void main(String[]a){SpringApplication.run(ZhuaTechCronOpsApplication.class,a);}}
